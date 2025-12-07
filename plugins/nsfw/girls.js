3dc3e8ad5b403891383ce84e5fb00a81dbfd08ba
Sebas-MD <169330308+sebas-mod@users.noreply.github.com>
2025-12-06 21:45:28 -0300

    Create girls.js

---
+++ plugins/nsfw/girls.js
@@ -0,0 +1,39 @@
+import fetch from 'node-fetch'
+
+var handler = async (m, { conn, usedPrefix, command }) => {
+
+    m.reply("👩‍🦰 *Buscando una chica random...*");
+
+    try {
+
+        let api = `https://api.soymaycol.icu/girls?apikey=may-dcfcae78`;
+
+        let res = await fetch(api);
+        let json = await res.json();
+
+        if (!json.status || !json.url) {
+            return m.reply("❌ No se pudo obtener la imagen.");
+        }
+
+        let img = json.url;
+
+        await conn.sendMessage(
+            m.chat,
+            {
+                image: { url: img },
+                caption: "✨ *Imagen random de una chica*"
+            },
+            { quoted: m }
+        );
+
+    } catch (e) {
+        console.error(e);
+        return m.reply("❌ Error al obtener la imagen.");
+    }
+};
+
+handler.help = ['girls'];
+handler.tags = ['nsfw'];
+handler.command = /^(girls|girl)$/i;
+
+export default handler;
